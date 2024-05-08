069167b4843bceddc2e7b204ef732ef174561d0a
mateuszurban <65718811+mateuszurban@users.noreply.github.com>
2024-05-08 13:44:41 +0200

    Fix vercel nextjs localhost undefined error

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -2,15 +2,24 @@
 
 import CartItems from "@/components/CartItems";
 import { PRODUCTS } from "../../constants/products";
+import { useEffect, useState } from "react";
 
 export default function Cart() {
   let products = PRODUCTS;
-  let localStorageCartItems = JSON.parse(localStorage.getItem("cart"));
+
+  // This is probably not an ideal way to fix "localstorage vs nextjs", but it's enough for demonstration purposes.
+  const [cartItems, setCartItems] = useState("");
+
+  useEffect(() => {
+    let localStorageCartItems;
+    localStorageCartItems = JSON.parse(localStorage.getItem("cart"));
+    setCartItems(localStorageCartItems);
+  }, []);
 
   // Filters "in" items from PRODUCTS, it they match localStorage id's
   // In proper app, this would probably be an API call for products with a matching ID?
   let cartProducts = products.filter((product) =>
-    localStorageCartItems.includes(product.id)
+    cartItems.includes(product.id)
   );
 
   return (
--- src/app/favourites/page.js
+++ src/app/favourites/page.js
@@ -2,15 +2,24 @@
 
 import FavouritesList from "@/components/FavouritesList";
 import { PRODUCTS } from "../../constants/products";
+import { useEffect, useState } from "react";
 
 export default function Favourites() {
   let products = PRODUCTS;
-  let localStorageFavourites = JSON.parse(localStorage.getItem("favourites"));
+
+  const [favouriteItems, setFavouriteItems] = useState("");
+  useEffect(() => {
+    let localStorageFavourites;
+    localStorageFavourites = JSON.parse(localStorage.getItem("favourites"));
+    setFavouriteItems(localStorageFavourites);
+  }, []);
+
+  // let localStorageFavourites = JSON.parse(localStorage.getItem("favourites"));
 
   // Filters "in" items from PRODUCTS, it they match localStorage id's
   // In proper app, this would probably be an API call for products with a matching ID?
   let favouriteProducts = products.filter((product) =>
-    localStorageFavourites.includes(product.id)
+    favouriteItems.includes(product.id)
   );
 
   return (
